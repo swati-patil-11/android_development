1a6477d18490fd2993ded2693c8f704c678f9a77
swati-patil-11 <swatiipatil12@gmail.com>
2026-04-12 23:07:14 +0530

    Add files via upload

---
+++ Layouts/Table_Layout/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.tablelayout;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+// Added missing import for View
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Ensure that the root layout in activity_main.xml has the id: android:id="@+id/main"
+        View mainView = findViewById(R.id.main);
+
+        if (mainView != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
+    }
+}
